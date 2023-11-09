4e4c84dfbdf016d8ce5744bd8e74262979851505
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-11-09 14:14:47 +0900

    First Commit

---
+++ Madii/MadiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadiiApp.swift
+//  Madii
+//
+//  Created by 이안진 on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MadiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
