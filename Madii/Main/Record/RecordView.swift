72ca77c9477fbae38e124b888d5955fe7b085991
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-11-30 17:28:52 +0900

    Feat(record): 둥근 모서리 배경 ViewModifier로

--- Madii/Main/Record/RecordView.swift
+++ Madii/Main/Record/RecordView.swift
@@ -13,32 +13,70 @@ struct RecordView: View {
         VStack(alignment: .leading, spacing: 0) {
             title
                 .padding(.horizontal, 22)
+                .padding(.bottom, 12)
             
-            VStack(alignment: .leading, spacing: 20) {
-                Text("나만의 소확행을 수집해보세요")
-                    .madiiFont(font: .madiiSubTitle, color: .white)
-                
-                HStack(spacing: 0) {
-                    TextField("누워서 빗소리 감상하기", text: $myNewJoy)
-                        .madiiFont(font: .madiiBody3, color: .white, withHeight: true)
+            VStack(alignment: .leading, spacing: 16) {
+                // 나만의 소확행을 수집해보세요
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("나만의 소확행을 수집해보세요")
+                        .madiiFont(font: .madiiSubTitle, color: .white)
                     
-                    Button {
+                    HStack(spacing: 0) {
+                        TextField("누워서 빗소리 감상하기", text: $myNewJoy)
+                            .madiiFont(font: .madiiBody3, color: .white, withHeight: true)
                         
-                    } label: {
-                        Rectangle()
-                            .frame(width: 36, height: 36)
+                        Button {
+                            // save joy
+                        } label: {
+                            // TODO: 저장 버튼 구현
+                            Rectangle()
+                                .frame(width: 36, height: 36)
+                        }
                     }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 16)
+                    // FIXME: Color System에 없는 색상 -> 추후 추가해서 넣기
+                    .background(Color(red: 0.21, green: 0.22, blue: 0.29))
+                    .cornerRadius(6)
                 }
-                .padding(.vertical, 10)
-                .padding(.horizontal, 16)
-                // FIXME: Color System에 없는 색상 -> 추후 추가해서 넣기
-                .background(Color(red: 0.21, green: 0.22, blue: 0.29))
-                .cornerRadius(6)
+                .padding(20)
+                .roundBackground()
+                
+                // 최근 & 많이 실천한 소확행
+                HStack(spacing: 12) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        Rectangle()
+                            .fill(Color.gray400)
+                            .frame(width: 36, height: 36)
+                        
+                        HStack {
+                            Text("최근 실천한 소확행")
+                                .madiiFont(font: .madiiBody2, color: .white)
+                            Spacer()
+                        }
+                    }
+                    .padding(16)
+                    .padding(.leading, 4)
+                    .roundBackground()
+                    
+                    VStack(alignment: .leading, spacing: 12) {
+                        Rectangle()
+                            .fill(Color.gray400)
+                            .frame(width: 36, height: 36)
+                        
+                        HStack {
+                            Text("많이 실천한 소확행")
+                                .madiiFont(font: .madiiBody2, color: .white)
+                            Spacer()
+                        }
+                    }
+                    .padding(16)
+                    .padding(.leading, 4)
+                    .roundBackground()
+                }
+                
             }
-            .padding(20)
-            // FIXME: Color System에 없는 색상 -> 추후 추가해서 넣기
-            .background(Color(red: 0.1, green: 0.1, blue: 0.15))
-            .cornerRadius(20)
+            // 화면 전체 좌우 여백 16
             .padding(.horizontal, 16)
             
             Spacer()
