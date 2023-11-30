c5fba905ead506032fe761d0d9308eecb6ac8332
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-11-30 18:33:38 +0900

    Fix(tab-bar): 키보드랑 탭바가 같이 올라가는 문제 해결

--- Madii/Main/TabView/MadiiTabView.swift
+++ Madii/Main/TabView/MadiiTabView.swift
@@ -9,18 +9,36 @@ import SwiftUI
 
 struct MadiiTabView: View {
     @State var tabIndex: TabIndex = .record
-    
+    @State var isKeyboardShown: Bool = false
+
     var body: some View {
         NavigationStack {
-            ZStack(alignment: .bottom) {
+            VStack(spacing: 0) {
                 switch tabIndex {
-                case .home: HomeView()
+                case .home: HomeView().padding(.bottom, 60)
                 case .record: RecordView()
-                case .calendar: CalendarView()
+                case .calendar: CalendarView().padding(.bottom, 60)
                 }
-                
-                MadiiTabBar(tabIndex: $tabIndex)
+
+                // 키보드가 올라오면 함께 올라오는 문제 해결을 위해
+                // 키보드가 없을 때만 나타나도록 구현
+                if isKeyboardShown == false {
+                    MadiiTabBar(tabIndex: $tabIndex)
+                }
             }
+            .onAppear {
+                checkIsKeyboardShown()
+            }
+        }
+    }
+
+    func checkIsKeyboardShown() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { _ in
+            isKeyboardShown = true
+        }
+
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
+            isKeyboardShown = false
         }
     }
 }
