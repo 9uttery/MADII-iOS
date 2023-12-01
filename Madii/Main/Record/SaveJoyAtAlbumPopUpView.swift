44ccbc9a349dd7d7cb3497b965849053918894d9
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-01 17:55:58 +0900

    Add(pop-up): select album row 추가

--- Madii/Main/Record/SaveJoyAtAlbumPopUpView.swift
+++ Madii/Main/Record/SaveJoyAtAlbumPopUpView.swift
@@ -74,27 +74,6 @@ struct SaveJoyAtAlbumPopUpView: View {
     func rightA() {}
 }
 
-struct SelectAlbumRow: View {
-    let title: String
-    let isSelected: Bool
-
-    var body: some View {
-        HStack {
-            Text(title)
-                .madiiFont(font: .madiiBody3, color: isSelected ? .white : .gray500)
-            Spacer()
-        }
-        .padding(.horizontal, 6)
-        .padding(.vertical, 8)
-        .background(Color(red: 0.21, green: 0.22, blue: 0.29))
-        .cornerRadius(4)
-        .overlay(
-            RoundedRectangle(cornerRadius: 4)
-                .stroke(Color.madiiYellowGreen.opacity(isSelected ? 1.0 : 0.0), lineWidth: 1)
-        )
-    }
-}
-
 #Preview {
     RecordView()
 }
