8e2da45e7b408e6149ffa41373778f08f9ad6d2a
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-05 19:54:53 +0900

    Feat(calendar): 캘린더 상단 타이틀 UI

--- Madii/Main/Calendar/CalendarView.swift
+++ Madii/Main/Calendar/CalendarView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct CalendarView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                Text("calendar")
-                Spacer()
+        ZStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    title
+                    
+                    Spacer()
+                }
+                // 하단 여백 40
+                .padding(.bottom, 40)
             }
-            Spacer()
+            .scrollIndicators(.hidden)
         }
+        .navigationTitle("")
+    }
+    
+    var title: some View {
+        HStack(spacing: 0) {
+            Text("캘린더")
+                .madiiFont(font: .madiiTitle, color: .white)
+                .padding(.vertical, 12)
+            
+            Spacer()
+            
+            NavigationLink {
+                Text("프로필")
+            } label: {
+                Image(systemName: "person.crop.circle")
+                    .resizable()
+                    .frame(width: 22, height: 22)
+                    .foregroundStyle(Color.gray500)
+            }
+        }
+        .padding(.horizontal, 22)
+        .padding(.bottom, 12)
     }
 }
 
