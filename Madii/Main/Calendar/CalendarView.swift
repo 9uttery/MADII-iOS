9aa97134a5b4c200724d6161418927c32e1655f5
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-06 21:16:53 +0900

    Feat(calendar): 캘린더 전체 UI 레이아웃

--- Madii/Main/Calendar/CalendarView.swift
+++ Madii/Main/Calendar/CalendarView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @State private var selectedDate = Date()
     let weekdays = ["일", "월", "화", "수", "목", "금", "토"]
     
     var body: some View {
@@ -15,21 +16,46 @@ struct CalendarView: View {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
                     title
+                    weekdaysHeader
+                        .padding(.bottom, 12)
                     
-                    HStack(spacing: 0) {
-                        ForEach(weekdays, id: \.self) { weekday in
-                            Text(weekday)
-                                .madiiFont(font: .madiiBody3, color: .gray100)
-                                .padding(.horizontal, 14)
-                                .padding(.vertical, 10)
-                            
-                            Spacer()
+                    LazyVGrid(columns: Array(repeating: GridItem(), count: 7), spacing: 10) {
+                        ForEach(0 ..< countWeekday(), id: \.self) { _ in
+                            Rectangle()
+                                .foregroundStyle(Color.blue)
+                                .frame(width: 42, height: 98)
+                        }
+                        
+                        ForEach(monthDates(), id: \.self) { date in
+                            VStack(spacing: 0) {
+                                // 일자
+                                Text(date.day)
+                                    .frame(width: 42, height: 42)
+                                    .background(date.isSameDay(as: selectedDate) ? Color.blue : Color.gray)
+                                //                                .clipShape(Circle())
+                                    .onTapGesture {
+                                        selectedDate = date
+                                    }
+                                
+                                // 각 일마다 있는 소확행 커버
+                                LazyVGrid(columns: Array(repeating: GridItem(spacing: 3), count: 3), spacing: 3) {
+                                    ForEach(0 ..< 9, id: \.self) { i in
+                                        Circle()
+                                    }
+                                }
+                                .frame(width: 42)
+                                .background(Color.brown)
+                                
+                                Spacer()
+                            }
+                            .frame(height: 98)
                         }
                     }
-                    .padding(.horizontal, 22)
+                    .padding(.horizontal, 26)
                     
                     Spacer()
                 }
+                .background(Color.red)
                 // 하단 여백 40
                 .padding(.bottom, 40)
             }
@@ -58,8 +84,64 @@ struct CalendarView: View {
         .padding(.horizontal, 22)
         .padding(.bottom, 12)
     }
+    
+    var weekdaysHeader: some View {
+        HStack(spacing: 0) {
+            ForEach(weekdays, id: \.self) { weekday in
+                Text(weekday)
+                    .madiiFont(font: .madiiBody3, color: .gray100)
+                    .frame(width: 42, height: 42)
+                    .background(Color.green)
+                
+                if weekday != weekdays.last {
+                    Spacer()
+                }
+            }
+        }
+        .padding(.horizontal, 26)
+    }
+    
+    func monthDates() -> [Date] {
+        let calendar = Calendar.current
+        let monthRange = calendar.range(of: .day, in: .month, for: selectedDate)!
+        let startDate = calendar.date(from: calendar.dateComponents([.year, .month], from: selectedDate))!
+
+        return (0 ..< monthRange.count).map { calendar.date(byAdding: .day, value: $0, to: startDate)! }
+    }
+
+    func countWeekday() -> Int {
+        let calendar = Calendar.current
+        let startDate = calendar.date(from: calendar.dateComponents([.year, .month], from: selectedDate))!
+        let weekdayOfStartDate = calendar.component(.weekday, from: startDate)
+
+        return weekdayOfStartDate - 1
+    }
 }
 
 #Preview {
     CalendarView()
+}
+
+extension Date {
+    var day: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d"
+        return dateFormatter.string(from: self)
+    }
+    
+    var month: String {
+        let calendar = Calendar.current
+        let month = calendar.component(.month, from: self)
+        return "\(month)"
+    }
+    
+    var year: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy" // "yyyy"는 4자리 연도를 나타내는 포맷입니다
+        return dateFormatter.string(from: self)
+    }
+    
+    func isSameDay(as date: Date) -> Bool {
+        Calendar.current.isDate(self, inSameDayAs: date)
+    }
 }
