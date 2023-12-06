3f2fd093527433956a31eb991628c2b8dfdcfe05
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-06 22:52:06 +0900

    Feat(calendar): 날짜 cell의 색상 전달 함수

--- Madii/Main/Calendar/CalendarView.swift
+++ Madii/Main/Calendar/CalendarView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct CalendarView: View {
-    let today = Date()
     @State private var selectedDate = Date()
     let weekdays = ["일", "월", "화", "수", "목", "금", "토"]
     
@@ -25,47 +24,8 @@ struct CalendarView: View {
                     // 요일 header
                     weekdaysHeader.padding(.bottom, 12)
                     
-                    LazyVGrid(columns: Array(repeating: GridItem(), count: 7), spacing: 10) {
-                        ForEach(0 ..< countWeekday(), id: \.self) { _ in
-                            Rectangle()
-                                .foregroundStyle(Color.clear)
-                                .frame(width: 42, height: 98)
-                        }
-                        
-                        ForEach(monthDates(), id: \.self) { date in
-                            VStack(spacing: 0) {
-                                // 일자
-                                Text(date.day)
-                                    .madiiFont(font: .madiiTitle, color: (date.isSameDay(as: today) && selectedDate.day != today.day) ? .madiiOrange : .white)
-                                    .frame(width: 36, height: 36)
-                                    .background(date.isSameDay(as: selectedDate) ? Color.madiiOrange : Color.clear)
-                                    .overlay(
-                                        Circle()
-                                            .strokeBorder(date.isSameDay(as: today) ? Color.madiiOrange : Color.clear, lineWidth: 1)
-                                    )
-                                    .clipShape(Circle())
-                                    .frame(width: 42, height: 42)
-                                    .onTapGesture {
-                                        selectedDate = date
-                                    }
-                                
-                                // 각 일마다 있는 소확행 커버
-                                let count = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].randomElement() ?? 9
-                                LazyVGrid(columns: Array(repeating: GridItem(spacing: 3), count: 3), spacing: 3) {
-                                    ForEach(0 ..< count, id: \.self) { _ in
-                                        let colors = [Color.madiiPurple, Color.madiiOrange, Color.teal]
-                                        Circle()
-                                            .fill(colors.randomElement() ?? Color.madiiPurple)
-                                    }
-                                }
-                                .frame(width: 42)
-                                
-                                Spacer()
-                            }
-                            .frame(height: 98)
-                        }
-                    }
-                    .padding(.horizontal, 26)
+                    // 일 (캘린더 일자)
+                    CalendarDays(selectedDate: $selectedDate)
                     
                     Spacer()
                 }
@@ -111,22 +71,6 @@ struct CalendarView: View {
             }
         }
         .padding(.horizontal, 26)
-    }
-    
-    func monthDates() -> [Date] {
-        let calendar = Calendar.current
-        let monthRange = calendar.range(of: .day, in: .month, for: selectedDate)!
-        let startDate = calendar.date(from: calendar.dateComponents([.year, .month], from: selectedDate))!
-
-        return (0 ..< monthRange.count).map { calendar.date(byAdding: .day, value: $0, to: startDate)! }
-    }
-
-    func countWeekday() -> Int {
-        let calendar = Calendar.current
-        let startDate = calendar.date(from: calendar.dateComponents([.year, .month], from: selectedDate))!
-        let weekdayOfStartDate = calendar.component(.weekday, from: startDate)
-
-        return weekdayOfStartDate - 1
     }
 }
 
