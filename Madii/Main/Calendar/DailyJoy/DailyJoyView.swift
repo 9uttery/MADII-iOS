d13bd8849185fbaf7eb1aae2a6e50ab0485903d5
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-07 17:18:21 +0900

    Add(daily-joy): Calendar 메인에서 쓰이는 파일 묶고 daily joy view 파일 생성

---
+++ Madii/Main/Calendar/DailyJoy/DailyJoyView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyJoyView.swift
+//  Madii
+//
+//  Created by 이안진 on 12/7/23.
+//
+
+import SwiftUI
+
+struct DailyJoyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyJoyView()
+}
