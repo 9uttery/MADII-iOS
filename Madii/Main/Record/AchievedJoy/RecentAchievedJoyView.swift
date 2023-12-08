c62385057bde0db62965e296f647de3ce881a884
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-08 16:44:33 +0900

    Feat(daily-joy): DailyJoy 화면에서 쓰일 소확행 dummy 추가

--- Madii/Main/Record/AchievedJoy/RecentAchievedJoyView.swift
+++ Madii/Main/Record/AchievedJoy/RecentAchievedJoyView.swift
@@ -7,26 +7,6 @@
 
 import SwiftUI
 
-struct RecentAchievedJoy: Identifiable {
-    let id = UUID()
-    let date: String
-    let joys: [Joy]
-    
-    static let dummys: [RecentAchievedJoy] = [RecentAchievedJoy(date: "2023.12.25", joys: [Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기")]), RecentAchievedJoy(date: "2023.12.24", joys: [Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기")]), RecentAchievedJoy(date: "2023.12.23", joys: [Joy(title: "넷플릭스 보면서 귤 까먹기")])]
-}
-
-struct Joy: Identifiable {
-    let id = UUID()
-    let title: String
-    var counts: Int = 0
-    
-    static let manyAchievedDummy: [Joy] = [Joy(title: "샤브샤브 먹기", counts: 21),
-                                           Joy(title: "방어 먹기", counts: 17),
-                                           Joy(title: "열라면 먹기", counts: 12),
-                                           Joy(title: "연어 먹기", counts: 5),
-                                           Joy(title: "돈까스 먹기", counts: 3)]
-}
-
 struct RecentAchievedJoyView: View {
     @State private var allJoys: [RecentAchievedJoy] = RecentAchievedJoy.dummys
     
