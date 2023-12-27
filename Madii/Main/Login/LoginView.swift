6f1e78e881e57991ee668180531af9f1703945fe
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-12-27 14:22:04 +0900

    Feat(login): 카카오 로그인 테스트를 위한 화면

---
+++ Madii/Main/Login/LoginView.swift
@@ -0,0 +1,42 @@
+//
+//  LoginView.swift
+//  Madii
+//
+//  Created by 이안진 on 12/27/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            HStack { Spacer() }
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("카카오 로그인")
+                    .font(.title.bold())
+            }
+            
+            Spacer()
+        }
+        .background( background() )
+    }
+    
+    func background() -> LinearGradient {
+        LinearGradient(
+            stops: [
+                Gradient.Stop(color: Color(red: 0.09, green: 0.09, blue: 0.15), location: 0.00),
+                Gradient.Stop(color: Color(red: 0.42, green: 0.44, blue: 0.68), location: 1.00)
+            ],
+            startPoint: UnitPoint(x: 0.5, y: 0),
+            endPoint: UnitPoint(x: 0.5, y: 1)
+        )
+    }
+}
+
+#Preview {
+    LoginView()
+}
