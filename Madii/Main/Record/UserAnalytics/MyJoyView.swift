7d545936edb4005248eb9046ce536557c57d5e94
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-01-25 16:16:56 +0900

    Feat(R-레코드): 소확행 메뉴 bottom sheet 추가

--- Madii/Main/Record/UserAnalytics/MyJoyView.swift
+++ Madii/Main/Record/UserAnalytics/MyJoyView.swift
@@ -8,36 +8,18 @@
 import SwiftUI
 
 struct MyJoyView: View {
-//    @State private var allJoys: [MyJoy] = MyJoy.dummys
-    @State private var allJoys: [MyJoy] = []
-    @State private var selectedJoy: Joy?
+    @State private var allJoys: [MyJoy] = MyJoy.dummys
+//    @State private var allJoys: [MyJoy] = []
+    @State private var selectedJoy: Joy? = MyJoy.dummys[0].joys[0]
     
+    @State private var sheetHeight: CGFloat = .zero
+
     var body: some View {
         VStack(spacing: 0) {
             if allJoys.isEmpty {
                 // 내가 기록한 소확행 없을 때
-                VStack(spacing: 60) {
-                    Rectangle()
-                        .frame(width: 200, height: 200)
-                    
-                    VStack(spacing: 16) {
-                        Text("기록된 소확행이 없어요.\n소확행을 기록하고 행복을 충전하세요!")
-                            .madiiFont(font: .madiiBody3, color: .gray500)
-                            .multilineTextAlignment(.center)
-                        
-                        Button {
-                            // TODO: 레코드 탭 화면으로 넘어가서 소확행 기록 박스 테두리
-                        } label: {
-                            Text("소확행 기록하러 가기")
-                                .madiiFont(font: .madiiBody1, color: Color(red: 0.51, green: 0.68, blue: 0.02))
-                                .padding(.horizontal, 24)
-                                .padding(.vertical, 12)
-                                .background(Color.madiiYellowGreen)
-                                .clipShape(RoundedRectangle(cornerRadius: 90))
-                        }
-                    }
-                }
-            } else {               
+                emptyJoyView
+            } else {
                 // 내가 기록한 소확행 있을 때
                 ScrollView {
                     VStack(spacing: 20) {
@@ -46,9 +28,7 @@ struct MyJoyView: View {
                             joyBoxByDate(eachDayJoy.date, joys: eachDayJoy.joys)
                         }
                         .sheet(item: $selectedJoy) { item in
-                            Text(item.title)
-                                .presentationDetents([.medium])
-                        }
+                            JoyMenuBottomSheet(joy: item, isMine: true) }
                     }
                     .padding(.top, 28)
                     .padding(.horizontal, 16)
@@ -60,6 +40,30 @@ struct MyJoyView: View {
         .navigationTitle("내가 기록한 소확행")
         .toolbarBackground(Color.madiiBox, for: .navigationBar)
         .toolbarBackground(.visible, for: .navigationBar)
+    }
+    
+    private var emptyJoyView: some View {
+        VStack(spacing: 60) {
+            Rectangle()
+                .frame(width: 200, height: 200)
+            
+            VStack(spacing: 16) {
+                Text("기록된 소확행이 없어요.\n소확행을 기록하고 행복을 충전하세요!")
+                    .madiiFont(font: .madiiBody3, color: .gray500)
+                    .multilineTextAlignment(.center)
+                
+                Button {
+                    // TODO: 레코드 탭 화면으로 넘어가서 소확행 기록 박스 테두리
+                } label: {
+                    Text("소확행 기록하러 가기")
+                        .madiiFont(font: .madiiBody1, color: Color(red: 0.51, green: 0.68, blue: 0.02))
+                        .padding(.horizontal, 24)
+                        .padding(.vertical, 12)
+                        .background(Color.madiiYellowGreen)
+                        .clipShape(RoundedRectangle(cornerRadius: 90))
+                }
+            }
+        }
     }
     
     @ViewBuilder
--- Madii/Model/Joy.swift
+++ Madii/Model/Joy.swift
@@ -35,7 +35,7 @@ struct MyJoy: Identifiable {
     let date: String
     let joys: [Joy]
     
-    static let dummys: [MyJoy] = [MyJoy(date: "2023.12.25", joys: [Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기")]), 
+    static let dummys: [MyJoy] = [MyJoy(date: "2023.12.25", joys: [Joy(title: "뜨끈한 메밀차 마시기 뜨끈한 메밀차 마시기 뜨끈한 메밀"), Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기")]), 
                                   MyJoy(date: "2023.12.24", joys: [Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기")]),
                                   MyJoy(date: "2023.12.23", joys: [Joy(title: "넷플릭스 보면서 귤 까먹기")]),
                                   MyJoy(date: "2023.12.22", joys: [Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기"), Joy(title: "넷플릭스 보면서 귤 까먹기")])]
