1653ee069f8c9c5a0b8a905fce4e568c3ecb601a
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-01-25 17:46:59 +0900

    Feat(H5-행복을 재생해요): 행복을 재생해요 List에 필요한 더미 데이터 추가

--- Madii/Model/Album.swift
+++ Madii/Model/Album.swift
@@ -27,4 +27,17 @@ struct Album: Identifiable {
                                   Album(id: 2, title: "비 올 때 하기 좋은 소확행", creator: "에몽", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
                                   Album(id: 3, title: "겨울 필수 소확행 모음 ZIP", creator: "도요", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
                                   Album(id: 4, title: "샤브샤브 먹고 싶어", creator: "하노", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요.")]
+    static let dummy5: [Album] = [Album(id: 1, title: "무더웠던 여름이 지나가고 다가오는 가을..?", creator: "구떠리", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 2, title: "비 올 때 하기 좋은 소확행", creator: "에몽", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 3, title: "겨울 필수 소확행 모음 ZIP", creator: "도요", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 4, title: "샤브샤브 먹고 싶어", creator: "하노", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."), 
+                                  Album(id: 5, title: "무더웠던 여름이 지나가고 다가오는 가을..?", creator: "구떠리", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 6, title: "비 올 때 하기 좋은 소확행", creator: "에몽", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 7, title: "겨울 필수 소확행 모음 ZIP", creator: "도요", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 8, title: "샤브샤브 먹고 싶어", creator: "하노", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 9, title: "무더웠던 여름이 지나가고 다가오는 가을..?", creator: "구떠리", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 10, title: "비 올 때 하기 좋은 소확행", creator: "에몽", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 11, title: "겨울 필수 소확행 모음 ZIP", creator: "도요", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요."),
+                                  Album(id: 12, title: "샤브샤브 먹고 싶어", creator: "하노", description: "이 소확행은 기분이 째질 때 츄라이해보면 좋은 소확행이에요.")]
+    
 }
