5c8e46a9a20962c7c7ac29546890519d33abeffc
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-01-25 17:48:04 +0900

    Add(H4-1-취향저격 소확행): 취향저격 소확행 화면에서 필요한 더미 데이터 추가

--- Madii/Model/Joy.swift
+++ Madii/Model/Joy.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 
-struct Joy: Identifiable {
+struct Joy: Identifiable, Equatable {
     let id = UUID()
     let title: String
     var counts: Int = 0
     var satisfaction: Float = 100.0
+    var weather: Int = 1
+    var with: Int = 1
+    var happy: Int = 1
     
     static let manyAchievedDummy: [Joy] = [Joy(title: "샤브샤브 먹기", counts: 21),
                                            Joy(title: "방어 먹기", counts: 17),
@@ -23,6 +26,30 @@ struct Joy: Identifiable {
         Joy(title: "산책하면서 크리스마스 플리 듣기", satisfaction: 80.0),
         Joy(title: "샤워하고 아이스크림 먹기", satisfaction: 65.0),
         Joy(title: "전기장판에 누워서 귤 까먹기", satisfaction: 90.0)]
+    
+    static let styleJoyDummy: [Joy] = [
+        Joy(title: "산책하면서 크리스마스 플리 듣기", weather: 1, with: 2, happy: 3),
+        Joy(title: "샤워하고 아이스크림 먹기", weather: 3, with: 2, happy: 1),
+        Joy(title: "전기장판에 누워서 귤 까먹기", weather: 2, with: 1, happy: 3),
+        Joy(title: "목욕하면서 크리스마스 플리 듣기", weather: 2, with: 2, happy: 2),
+        Joy(title: "샤워하고 붕어빵 먹기", weather: 2, with: 2, happy: 1),
+        Joy(title: "전기장판에 누워서 밤 까먹기", weather: 1, with: 2, happy: 3),
+        Joy(title: "산책하면서 부활절 플리 듣기", weather: 2, with: 1, happy: 1),
+        Joy(title: "목욕하고 아이스크림 먹기", weather: 2, with: 1, happy: 3),
+        Joy(title: "쿨매트에 누워서 귤 까먹기", weather: 3, with: 3, happy: 3),
+        Joy(title: "스키타면서 크리스마스 플리 듣기", weather: 2, with: 1, happy: 2),
+        Joy(title: "샤워하고 술 먹기", weather: 1, with: 1, happy: 1),
+        Joy(title: "전기장판에 서서 귤 까먹기", weather: 1, with: 1, happy: 2),
+        Joy(title: "산책하면서 크리스마스 플리 만들기", weather: 3, with: 2, happy: 3),
+        Joy(title: "샤워하고 아이스크림 사기", weather: 3, with: 2, happy: 1),
+        Joy(title: "전기장판에 누워서 귤 버리기", weather: 2, with: 3, happy: 3),
+        Joy(title: "코딩하면서 크리스마스 플리 듣기", weather: 3, with: 2, happy: 1),
+        Joy(title: "운동하고 아이스크림 먹기", weather: 2, with: 2, happy: 1),
+        Joy(title: "전기장판에 누워서 굴 까먹기", weather: 3, with: 1, happy: 2),
+        Joy(title: "산책하면서 한강 플리 듣기", weather: 2, with: 2, happy: 3),
+        Joy(title: "샤워하고 호떡 먹기", weather: 1, with: 2, happy: 2),
+        Joy(title: "전기장판에 누워서 귤 따기", weather: 2, with: 3, happy: 1),
+    ]
 }
 
 struct RecentAchievedJoy: Identifiable {
