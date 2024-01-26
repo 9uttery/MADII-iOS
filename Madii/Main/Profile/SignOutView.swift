a6176f1c23f1f1c84a78367f896b5bbc40b79d30
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-01-26 14:05:33 +0900

    Feat(H-홈화면): 취향저격 ->버튼 이미지 추가

---
+++ Madii/Main/Profile/SignOutView.swift
@@ -0,0 +1,18 @@
+//
+//  SignOutView.swift
+//  Madii
+//
+//  Created by 정태우 on 1/26/24.
+//
+
+import SwiftUI
+
+struct SignOutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignOutView()
+}
