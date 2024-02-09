1c91c2d88487330f2936e9e4dacbc2f158cb02a1
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-02-10 03:09:51 +0900

    Feat(L-로그인): 일반 로그인 api 화면 연결

--- Madii/Main/Login/Login/LoginWithIdView.swift
+++ Madii/Main/Login/Login/LoginWithIdView.swift
@@ -25,7 +25,7 @@ struct LoginWithIdView: View {
             
             Button {
                 // 로그인
-                showMainView = true
+                login()
             } label: {
                 MadiiButton(title: "다음", size: .big)
                     .opacity(isTextFieldAllFilled ? 1.0 : 0.4)
@@ -59,6 +59,25 @@ struct LoginWithIdView: View {
             
             MadiiTextField(isSecureField: true, placeHolder: "비밀번호를 입력해주세요", text: $password)
                 .padding(.horizontal, 8)
+        }
+    }
+    
+    // 로그인
+    private func login() {
+        UsersAPI.shared.loginWithId(id: id, password: password) { isSuccess, response in
+            if isSuccess {
+                // api 통신 성공
+                if response.hasProfile {
+                    // 프로필 저장 완료 -> 메인 화면으로
+                    showMainView = true
+                } else {
+                    // 프로필 저장 전 -> 프로필 설정 화면으로
+                    print("DEBUG(LoginWithIdView): login() hasProfile false")
+                }
+            } else {
+                // api 통신 실패 || 계정 정보 없음
+                print("DEBUG(LoginWithIdView): login() isSuccess false")
+            }
         }
     }
 }
