3f7721b0adb54e10c30e5c8c087fa376dffe5ea7
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-02-19 16:18:35 +0900

    Feat(H4- 취향저격 소확행): 취향저격 소확행 컴포넌트화

--- Madii/Main/Home/RecommendJoyListView.swift
+++ Madii/Main/Home/RecommendJoyListView.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct RecommendJoyListView: View {
+    @State private var selectedIdx: Int?
+    @Binding var recommendJoys: [GetJoyResponseJoy]
+    @State private var selectedJoy: GetJoyResponseJoy?
+    @Binding var isClicked: [Bool]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            if recommendJoys.isEmpty {
+                ForEach(1...3, id: \.self) {_ in
+                    HStack(spacing: 0) {
+                        Circle()
+                            .foregroundColor(.gray300)
+                            .frame(width: 56, height: 56)
+                            .padding(.trailing, 15)
+                        Rectangle()
+                            .foregroundColor(.gray300)
+                            .frame(width: 172, height: 20)
+                        Spacer()
+                        Image(systemName: "ellipsis")
+                    }
+                    .padding(20)
+                    .background(Color.madiiBox)
+                    .cornerRadius(20)
+                    .opacity(0.4)
+                }
+            } else {
+                ForEach(recommendJoys) { joy in
+                    Button {
+                        if selectedIdx == joy.joyId {
+                            selectedIdx = nil
+                        } else {
+                            selectedIdx = joy.id
+                        }
+                    } label: {
+                        HStack(spacing: 0) {
+                            Image("")
+                                .resizable()
+                                .frame(width: 56, height: 56)
+                                .background(Color.black)
+                                .cornerRadius(90)
+                                .padding(.trailing, 15)
+                            
+                            Text(joy.contents)
+                                .madiiFont(font: .madiiBody3, color: .white)
+                                .multilineTextAlignment(.leading)
+                            
+                            Spacer()
+                            
+                            Button {
+                                selectedJoy = joy
+                            } label: {
+                                VStack {
+                                    Image(systemName: "ellipsis")
+                                }
+                                .frame(width: 40, height: 40)
+                            }
+                        }
+                        .roundBackground()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .inset(by: 0)
+                                .stroke(joy.joyId == selectedIdx ? Color.madiiYellowGreen : Color.clear, lineWidth: 2)
+                        )
+                    }
+                }
+//                .sheet(item: $selectedJoy) { item in
+//                    JoyMenuBottomSheet(joy: item, isMine: true) }
+            }
+            Spacer()
+            NavigationLink {
+                
+            } label: {
+                StyleJoyNextButton(label: "완료", isDisabled: isClicked.contains(true))
+            }
+        }
     }
 }
 
 #Preview {
-    RecommendJoyListView()
+    RecommendJoyListView(recommendJoys: .constant([]), isClicked: .constant(Array(repeating: false, count: 9)))
 }
