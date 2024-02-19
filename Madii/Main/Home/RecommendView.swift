342b9fb5e454df179c537cc9225e8a0a7ee1bcdb
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-02-19 16:18:11 +0900

    Feat(H4- 취향저격 소확행): 화면 컴포넌트화와 API연동

--- Madii/Main/Home/RecommendView.swift
+++ Madii/Main/Home/RecommendView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct RecommendView: View {
-    @State private var selectedIdx: UUID?
     @State var nickName: String = "코코"
     @State var isClicked: [Bool] = Array(repeating: false, count: 9)
-    @State var recommendJoys: [Joy] = Joy.dailyJoyDummy
-    @State private var selectedJoy: Joy?
+    @State var recommendJoys: [GetJoyResponseJoy] = []
+    @State var when: [Int] = []
+    @State var who: [Int] = []
+    @State var which: [Int] = []
     var body: some View {
         VStack(spacing: 0) {
             Text("키워드를 선택해주세요")
@@ -21,95 +22,90 @@ struct RecommendView: View {
                 .padding(.bottom, 40)
             VStack(spacing: 12) {
                 HStack {
-                    StyleJoyButton(label: "화창한 날씨", isClicked: $isClicked[0], buttonColor: Color.madiiSkyBlue)
+                    StyleJoyButton(label: "화창한 날씨", isClicked: $isClicked[0], buttonColor: Color.madiiSkyBlue) {
+                        if isClicked[0] {
+                            when.append(1)
+                        } else {
+                            when.removeAll { $0 == 1 }
+                        }
+                    }
                     
-                    StyleJoyButton(label: "혼자서", isClicked: $isClicked[1], buttonColor: Color.madiiPink)
+                    StyleJoyButton(label: "혼자서", isClicked: $isClicked[1], buttonColor: Color.madiiPink) {
+                        if isClicked[1] {
+                            who.append(4)
+                        } else {
+                            who.removeAll { $0 == 4 }
+                        }
+                    }
                     
-                    StyleJoyButton(label: "특별한 도전을 할 수 있는", isClicked: $isClicked[2], buttonColor: Color.madiiOrange)
+                    StyleJoyButton(label: "특별한 도전을 할 수 있는", isClicked: $isClicked[2], buttonColor: Color.madiiOrange) {
+                        if isClicked[2] {
+                            which.append(7)
+                        } else {
+                            which.removeAll { $0 == 7 }
+                        }
+                    }
                 }
                 HStack {
-                    StyleJoyButton(label: "지금 바로 할 수 있는", isClicked: $isClicked[3], buttonColor: Color.madiiOrange)
+                    StyleJoyButton(label: "지금 바로 할 수 있는", isClicked: $isClicked[3], buttonColor: Color.madiiOrange) {
+                        if isClicked[3] {
+                            which.append(9)
+                        } else {
+                            which.removeAll { $0 == 9 }
+                        }
+                    }
                     
-                    StyleJoyButton(label: "비 오는 날씨", isClicked: $isClicked[4], buttonColor: Color.madiiSkyBlue)
+                    StyleJoyButton(label: "비 오는 날씨", isClicked: $isClicked[4], buttonColor: Color.madiiSkyBlue) {
+                        if isClicked[4] {
+                            when.append(2)
+                        } else {
+                            when.removeAll { $0 == 2 }
+                        }
+                    }
                     
-                    StyleJoyButton(label: "다 함께", isClicked: $isClicked[5], buttonColor: Color.madiiPink)
+                    StyleJoyButton(label: "다 함께", isClicked: $isClicked[5], buttonColor: Color.madiiPink) {
+                        if isClicked[5] {
+                            who.append(6)
+                        } else {
+                            who.removeAll { $0 == 6 }
+                        }
+                    }
                 }
                 HStack {
-                    StyleJoyButton(label: "눈 오는 날씨", isClicked: $isClicked[6], buttonColor: Color.madiiSkyBlue)
+                    StyleJoyButton(label: "눈 오는 날씨", isClicked: $isClicked[6], buttonColor: Color.madiiSkyBlue) {
+                        if isClicked[6] {
+                            when.append(3)
+                        } else {
+                            when.removeAll { $0 == 3 }
+                        }
+                    }
                     
-                    StyleJoyButton(label: "일상 속에서 할 수 있는", isClicked: $isClicked[7], buttonColor: Color.madiiOrange)
+                    StyleJoyButton(label: "일상 속에서 할 수 있는", isClicked: $isClicked[7], buttonColor: Color.madiiOrange) {
+                        if isClicked[7] {
+                            which.append(9)
+                        } else {
+                            which.removeAll { $0 == 9 }
+                        }
+                    }
                     
-                    StyleJoyButton(label: "둘이서", isClicked: $isClicked[8], buttonColor: Color.madiiPink)
+                    StyleJoyButton(label: "둘이서", isClicked: $isClicked[8], buttonColor: Color.madiiPink) {
+                        if isClicked[8] {
+                            who.append(5)
+                        } else {
+                            who.removeAll { $0 == 5 }
+                        }
+                    }
                 }
             }
             .padding(.bottom, 81)
-            VStack(spacing: 12) {
-                if recommendJoys.isEmpty {
-                    ForEach(1...3, id: \.self) {_ in
-                        HStack(spacing: 0) {
-                            Circle()
-                                .foregroundColor(.gray300)
-                                .frame(width: 56, height: 56)
-                                .padding(.trailing, 15)
-                            Rectangle()
-                                .foregroundColor(.gray300)
-                                .frame(width: 172, height: 20)
-                            Spacer()
-                            Image(systemName: "ellipsis")
-                        }
-                        .padding(20)
-                        .background(Color.madiiBox)
-                        .cornerRadius(20)
-                        .opacity(0.4)
+            RecommendJoyListView(recommendJoys: $recommendJoys, isClicked: $isClicked)
+        }
+        .onChange(of: isClicked) { newValue in
+            for (_, value) in newValue.enumerated() where value {
+                HomeAPI.shared.postJoyRecommend(when: when, who: who, which: which) { isSuccess, joyList in
+                    if isSuccess {
+                        recommendJoys = joyList
                     }
-                } else {
-                    ForEach(recommendJoys) { joy in
-                        Button {
-                            if selectedIdx == joy.id {
-                                selectedIdx = nil
-                            } else {
-                                selectedIdx = joy.id
-                            }
-                        } label: {
-                            HStack(spacing: 0) {
-                                Image("")
-                                    .resizable()
-                                    .frame(width: 56, height: 56)
-                                    .background(Color.black)
-                                    .cornerRadius(90)
-                                    .padding(.trailing, 15)
-                                
-                                Text(joy.title)
-                                    .madiiFont(font: .madiiBody3, color: .white)
-                                    .multilineTextAlignment(.leading)
-                                
-                                Spacer()
-                                
-                                Button {
-                                    selectedJoy = joy
-                                } label: {
-                                    VStack {
-                                        Image(systemName: "ellipsis")
-                                    }
-                                    .frame(width: 40, height: 40)
-                                }
-                            }
-                            .roundBackground()
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .inset(by: 0)
-                                    .stroke(joy.id == selectedIdx ? Color.madiiYellowGreen : Color.clear, lineWidth: 2)
-                            )
-                        }
-                    }
-                    .sheet(item: $selectedJoy) { item in
-                        JoyMenuBottomSheet(joy: item, isMine: true) }
-                }
-                Spacer()
-                NavigationLink {
-                    
-                } label: {
-                    StyleJoyNextButton(label: "완료", isDisabled: isClicked.contains(true))
                 }
             }
         }
