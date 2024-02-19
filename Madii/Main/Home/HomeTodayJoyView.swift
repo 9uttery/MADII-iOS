9f6bd7c415735dda1df5e829417044d4724461da
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-02-19 16:19:39 +0900

    Feat(H2-1- 오늘의 소확행): 오늘의 소확행 API 연동

--- Madii/Main/Home/HomeTodayJoyView.swift
+++ Madii/Main/Home/HomeTodayJoyView.swift
@@ -8,58 +8,84 @@
 import SwiftUI
 
 struct HomeTodayJoyView: View {
+    @State var counter = 0
     @EnvironmentObject private var tabBarManager: TabBarManager
-    @State private var todayJoy: String = "넷플릭스 보면서 귤 까먹기"
+    @State var todayJoy: GetJoyResponseJoy?
+    @State var todayJoyTitle: String = ""
     @State private var isClickedToday: Bool = false
     @State private var myNewJoy: String = "샤브샤브 먹고 싶어"
     @Binding var showSaveJoyPopUp: Bool
+    @State var todayJoyEllipsis: GetJoyResponseJoy?
     var body: some View {
         Image("madiiLogo")
             .resizable()
             .frame(width: 160, height: 22)
             .padding(.leading, 6)
             .padding(.vertical, 12)
-        VStack(alignment: .leading, spacing: 20) {
-            if !isClickedToday {
-                Button {
-                    isClickedToday.toggle()
-                } label: {
-                    HStack {
-                        Spacer()
-                        Text("클릭해보세요!")
-                            .madiiFont(font: .madiiBody1, color: .black)
-                            .padding(.vertical, 18)
-                        Spacer()
-                    }
-                    .background(isClickedToday ? Color.madiiYellowGreen : Color.madiiOrange)
-                    .cornerRadius(90)
-                }
-            } else {
-                HStack() {
-                    Image("")
-                        .resizable()
-                        .frame(width: 56, height: 56)
-                    Text(todayJoy)
-                        .madiiFont(font: .madiiBody3, color: .white)
-                    Spacer()
+        ZStack {
+            VStack(alignment: .leading, spacing: 20) {
+                if !isClickedToday {
                     Button {
-                        //                            tabBarManager.isTabBarShown = false
-                        showSaveJoyPopUp = true
+                        HomeAPI.shared.getJoyToday { isSuccess, todayJoy  in
+                            if isSuccess {
+                                self.todayJoy = todayJoy
+                                self.todayJoyTitle = todayJoy.contents
+                            }
+                        }
+                        isClickedToday.toggle()
+                        counter += 1
                     } label: {
-                        Image(myNewJoy.isEmpty ? "inactiveSave" : "activeSave")
-                            .resizable()
-                            .frame(width: 36, height: 36)
+                        ZStack {
+                            HStack {
+                                Spacer()
+                                Text("클릭해보세요!")
+                                    .madiiFont(font: .madiiBody1, color: .black)
+                                    .padding(.vertical, 18)
+                                Spacer()
+                            }
+                            .background(isClickedToday ? Color.madiiYellowGreen : Color.madiiOrange)
+                            .cornerRadius(90)
+                        }
                     }
-                    .disabled(myNewJoy.isEmpty)
+                } else {
+                    HStack {
+                        Image("play")
+                            .resizable()
+                            .frame(width: 56, height: 56)
+                        Text(todayJoyTitle)
+                            .madiiFont(font: .madiiBody3, color: .white)
+                        Spacer()
+                        Button {
+                            // tabBarManager.isTabBarShown = false
+                            showSaveJoyPopUp = true
+                        } label: {
+                            Image("play")
+                                .resizable()
+                                .foregroundColor(.gray300)
+                                .frame(width: 16, height: 18)
+                        }
+                        Button {
+                            guard let todayJoyEllipsis = todayJoy else { return }
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(.gray500)
+                                .rotationEffect(Angle(degrees: 90))
+                        }
+                            .sheet(item: $todayJoyEllipsis, content: { item in
+                                JoyMenuBottomSheet(joy: Joy(title: ""), isMine: false)
+                            })
+                    }
                 }
             }
+            .roundBackground("오늘의 소확행")
+            .padding(.top, 14)
+            .padding(.bottom, 16)
+            
+            ParticleView(counter: $counter)
         }
-        .roundBackground("오늘의 소확행")
-        .padding(.top, 14)
-        .padding(.bottom, 16)
     }
 }
 
-//#Preview {
-//    HomeTodayJoyView()
-//}
+#Preview {
+    HomeTodayJoyView(todayJoy: GetJoyResponseJoy(joyId: 0, joyIconNum: 0, contents: ""), showSaveJoyPopUp: .constant(true))
+}
