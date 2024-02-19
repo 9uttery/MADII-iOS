57eac95937fb3e0696a54d8c2d24238f456ef686
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-02-19 16:14:50 +0900

    Feat(H2-홈화면): 오늘의 소확행 파티클 디자인 구현

---
+++ Madii/Main/Home/RecommendJoyListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendJoyListView.swift
+//  Madii
+//
+//  Created by 정태우 on 2/18/24.
+//
+
+import SwiftUI
+
+struct RecommendJoyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendJoyListView()
+}
