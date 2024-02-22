c78a227844f319db68e2f648b583e79e617ef8bc
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-02-23 03:51:22 +0900

    Feat: 마이페이지 메인 화면

--- Madii/Main/Calendar/Profile/ProfileView.swift
+++ Madii/Main/Calendar/Profile/ProfileView.swift
@@ -9,104 +9,40 @@ import SwiftUI
 
 struct ProfileView: View {
     @State var name: String = "코코"
+    @State var url: String = ""
     @State var showLogOutPopUp: Bool = false
+    
     var body: some View {
         ZStack(alignment: .top) {
-            VStack(spacing: 16) {
-                NavigationLink {
-                    MyProfileView()
-                } label: {
-                    HStack {
-                        Image("")
-                            .resizable()
-                            .frame(width: 40, height: 40)
-                        Text(name)
-                            .madiiFont(font: .madiiSubTitle, color: .white)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray500)
-                            .imageScale(.small)
-                    }
-                    .roundBackground()
-                    .padding(.top, 28)
-                }
-                VStack(spacing: 10) {
-                    NavigationLink {
-                        
-                    } label: {
-                        HStack {
-                            Text("알림")
-                                .madiiFont(font: .madiiBody3, color: .white)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray500)
-                                .imageScale(.small)
-                        }
-                        .frame(height: 40)
-                    }
+            VStack(spacing: 18) {
+                NavigationLink { MyProfileView() } label: { userInfoRow }
+                    .background(Color.madiiBox)
+                    .cornerRadius(20)
+                
+                VStack(spacing: 4) {
+                    NavigationLink { } label: { profileRow(title: "알림") }
                     
-                    NavigationLink {
-                        NoticeView()
-                    } label: {
-                        HStack {
-                            Text("공지사항")
-                                .madiiFont(font: .madiiBody3, color: .white)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray500)
-                                .imageScale(.small)
-                        }
-                        .frame(height: 40)
-                    }
+                    NavigationLink { NoticeView() } label: { profileRow(title: "공지사항") }
                     
-                    NavigationLink {
-                        InquiryView()
-                    } label: {
-                        HStack {
-                            Text("문의하기")
-                                .madiiFont(font: .madiiBody3, color: .white)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray500)
-                                .imageScale(.small)
-                        }
-                        .frame(height: 40)
-                    }
+                    NavigationLink { InquiryView() } label: { profileRow(title: "문의하기") }
                 }
-                .roundBackground()
+                .padding(.vertical, 4)
+                .background(Color.madiiBox)
+                .cornerRadius(20)
                 
                 VStack(spacing: 10) {
-                    Button {
-                        showLogOutPopUp = true
-                    } label: {
-                        HStack {
-                            Text("로그아웃")
-                                .madiiFont(font: .madiiBody3, color: .white)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray500)
-                                .imageScale(.small)
-                        }
-                        .frame(height: 40)
-                    }
+                    Button { showLogOutPopUp = true } label: { profileRow(title: "로그아웃") }
                     
-                    NavigationLink {
-                        SignOutView()
-                    } label: {
-                        HStack {
-                            Text("회원탈퇴")
-                                .madiiFont(font: .madiiBody3, color: .white)
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray500)
-                                .imageScale(.small)
-                        }
-                        .frame(height: 40)
-                    }
+                    NavigationLink { SignOutView() } label: { profileRow(title: "회원탈퇴") }
                 }
-                .roundBackground()
+                .padding(.vertical, 4)
+                .background(Color.madiiBox)
+                .cornerRadius(20)
+                
                 Spacer()
             }
+            .padding(.top, 28)
+            .padding(.horizontal, 16)
             .navigationTitle("마이페이지")
             .toolbarBackground(Color.madiiBox, for: .navigationBar)
             .toolbarBackground(.visible, for: .navigationBar)
@@ -114,17 +50,52 @@ struct ProfileView: View {
                 ProfileAPI.shared.getUsersProfile { isSuccess, userProfile in
                     if isSuccess {
                         name = userProfile.nickname
+                        url = userProfile.image
                     }
                 }
             }
             
-            
-            if showLogOutPopUp {
-                LogOutPopUpView(showLogOutPopUp: $showLogOutPopUp)
-            }
+            if showLogOutPopUp { LogOutPopUpView(showLogOutPopUp: $showLogOutPopUp) }
         }
     }
     
+    private var userInfoRow: some View {
+        HStack(spacing: 12) {
+            AsyncImage(url: URL(string: url)) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+            } placeholder: {
+                Image("defaultProfile")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+            }
+
+            Text(name)
+                .madiiFont(font: .madiiSubTitle, color: .white)
+            Spacer()
+            Image(systemName: "chevron.right")
+                .foregroundColor(.gray500)
+                .imageScale(.small)
+        }
+        .padding(16)
+        .padding(.leading, 6)
+    }
+    
+    @ViewBuilder
+    private func profileRow(title: String) -> some View {
+        HStack {
+            Text(title)
+                .madiiFont(font: .madiiBody3, color: .white)
+            Spacer()
+            Image(systemName: "chevron.right")
+                .foregroundColor(.gray500)
+                .imageScale(.small)
+        }
+        .padding(16)
+        .padding(.leading, 6)
+    }
 }
 
 #Preview {
