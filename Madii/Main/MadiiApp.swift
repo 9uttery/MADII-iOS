b6eafd2defe1b8206d56148978de1d7ffeef0ddb
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-02-24 05:27:56 +0900

    Feat: fcm 토큰 불러오기

--- Madii/Main/MadiiApp.swift
+++ Madii/Main/MadiiApp.swift
@@ -6,9 +6,11 @@
 //
 
 import FirebaseCore
+import FirebaseMessaging
 import KakaoSDKAuth
 import KakaoSDKCommon
 import SwiftUI
+import UserNotifications
 
 @main
 struct MadiiApp: App {
@@ -37,10 +39,44 @@ struct MadiiApp: App {
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // 파이어베이스 설정
+        FirebaseApp.configure()
+        
+        // 앱 실행 시 사용자에게 알림 허용 권한을 받음
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound] // 필요한 알림 권한을 설정
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        // UNUserNotificationCenterDelegate를 구현한 메서드를 실행시킴
+        application.registerForRemoteNotifications()
+        
+        // 파이어베이스 Meesaging 설정
+        Messaging.messaging().delegate = self
+        
+        return true
+    }
+}
 
-    return true
-  }
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("fcmToken: \(fcmToken)")
+        
+        // fcmToken UserDefaults에 저장
+        UserDefaults.standard.set(fcmToken, forKey: "fcmToken")
+        UserDefaults.standard.synchronize()
+
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+    }
 }
