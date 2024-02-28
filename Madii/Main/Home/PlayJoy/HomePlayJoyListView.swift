7609ba67e48f8de6502ee3bbf526ec3f7047230d
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-02-28 23:57:46 +0900

    Feat: 행복을 재생해요 화면 UI 반영

--- Madii/Main/Home/PlayJoy/HomePlayJoyListView.swift
+++ Madii/Main/Home/PlayJoy/HomePlayJoyListView.swift
@@ -23,25 +23,35 @@ struct HomePlayJoyListView: View {
                         }
                     }
                 }
-                .padding(.vertical, 28)
+                .padding(.top, 28)
+                
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundStyle(Color.madiiOption)
+                    .padding(.top, 80)
+                    .padding(.bottom, 40)
                 
                 Text("마지막이에요! 나만의 소확행 모음을 만들어보세요")
                     .madiiFont(font: .madiiBody4, color: .gray500)
                     .padding(.bottom, 8)
-                    NavigationLink {
-                        
-                    } label: {
-                        Text("나만의 소확행 앨범 만들기")
-                            .madiiFont(font: .madiiBody5, color: .darkYellowGreen)
-                            .padding(.vertical, 8)
-                            .padding(.horizontal, 20)
-                            .background(Color.madiiYellowGreen)
-                            .cornerRadius(90)
-                        
-                    }
+                
+                NavigationLink {
+                    
+                } label: {
+                    Text("나만의 소확행 앨범 만들기")
+                        .madiiFont(font: .madiiBody5, color: .darkYellowGreen)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 20)
+                        .background(Color.madiiYellowGreen)
+                        .cornerRadius(90)
+                }
             }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 44)
         }
-        .padding(.horizontal, 16)
+        .scrollIndicators(.never)
+        .navigationTitle("행복을 재생해요")
+        .navigationBarTitleDisplayMode(.inline)
         .onAppear {
             HomeAPI.shared.getAllAlbums(albumId: nil, size: 30) { isSuccess, allAlbum in
                 if isSuccess {
