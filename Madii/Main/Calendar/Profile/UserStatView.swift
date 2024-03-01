73cdc46ef39aafa0441f49653b6ebb0974493b1e
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-03-02 01:42:40 +0900

    Feat(ProfileView): 회원탈퇴 화면 유저 정보 API 연동 및 화면 구현

--- Madii/Main/Calendar/Profile/UserStatView.swift
+++ Madii/Main/Calendar/Profile/UserStatView.swift
@@ -8,8 +8,64 @@
 import SwiftUI
 
 struct UserStatView: View {
+    @State var nickname: String = "에몽"
+    @State var days: Int = 96
+    @State var joy: Int = 32
+    @State var play: Int = 231
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("\(nickname)님은")
+                    .madiiFont(font: .madiiTitle, color: .white)
+                    .padding(.bottom, 8)
+                Text("\(days)일 동안 일상 속에서")
+                    .madiiFont(font: .madiiTitle, color: .white)
+                    .padding(.bottom, 8)
+                Text("\(joy)개의 소학행을")
+                    .madiiFont(font: .madiiTitle, color: .white)
+                    .padding(.bottom, 8)
+                HStack {
+                    Text("\(play)번 재생해왔어요")
+                        .madiiFont(font: .madiiTitle, color: .white)
+                    Spacer()
+                }
+            }
+            .padding(.leading, 24)
+            .padding(.bottom, 80)
+            Image("CD")
+                .resizable()
+                .frame(width: 228, height: 228)
+            Image("CDShadow")
+                .resizable()
+                .frame(width: 237, height: 139)
+            Image("DownArrow")
+                .resizable()
+                .frame(width: 32, height: 80)
+        }
+        .id(0)
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        .background(
+            LinearGradient(
+                stops: [
+                    Gradient.Stop(color: Color(red: 0.16, green: 0.16, blue: 0.28), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.09, green: 0.09, blue: 0.15), location: 0.79),
+                    Gradient.Stop(color: Color(red: 0.1, green: 0.1, blue: 0.16), location: 1.00)
+                ],
+                startPoint: UnitPoint(x: 0.5, y: 0),
+                endPoint: UnitPoint(x: 0.5, y: 1.62)
+            )
+        )
+        .onAppear { getUserStat() }
+    }
+    private func getUserStat() {
+        ProfileAPI.shared.getUsersStat { isSuccess, userStat in
+            if isSuccess {
+                nickname = userStat.nickname
+                days = userStat.activeDays
+                joy = userStat.achievedJoyCount
+                play = userStat.achievementCount
+            }
+        }
     }
 }
 
