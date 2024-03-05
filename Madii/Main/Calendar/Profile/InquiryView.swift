569fa8ad60eb5f7437d735d8b59fcb71321367e5
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-03-05 11:51:00 +0900

    feat: 문의하기 뷰 이메일 앱 연동

--- Madii/Main/Calendar/Profile/InquiryView.swift
+++ Madii/Main/Calendar/Profile/InquiryView.swift
@@ -5,15 +5,25 @@
 //  Created by 정태우 on 1/26/24.
 //
 
+import MessageUI
 import SwiftUI
 
 struct InquiryView: View {
+    @State private var isShowingMailView = false
+    @State private var result: Result<MFMailComposeResult, Error>? = nil
     var body: some View {
         VStack {
-            Image("MailArrow")
-                .resizable()
-                .frame(width: 111, height: 85)
-                .padding(.bottom, 32)
+            Button {
+                isShowingMailView.toggle()
+            } label: {
+                Image("MailArrow")
+                    .resizable()
+                    .frame(width: 111, height: 85)
+                    .padding(.bottom, 32)
+            }
+            .sheet(isPresented: $isShowingMailView) {
+                MailView(isShowing: $isShowingMailView, result: $result)
+            }
             Text("madii.service.cs@gmail.com로 문의해주세요")
         }
         .navigationTitle("문의하기")
