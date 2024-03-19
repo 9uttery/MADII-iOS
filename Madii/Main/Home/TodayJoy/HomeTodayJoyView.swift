8bd2b8dfe378ddbc3f473d41527a5ed99670d55f
anjiniii <117328806+anjiniii@users.noreply.github.com>
2024-03-19 20:16:03 +0900

    Feat: 오늘의 소확행 get 해왔는지 여부 기록

--- Madii/Main/Home/TodayJoy/HomeTodayJoyView.swift
+++ Madii/Main/Home/TodayJoy/HomeTodayJoyView.swift
@@ -10,41 +10,46 @@ import SwiftUI
 struct HomeTodayJoyView: View {
 
     @State private var todayJoy: Joy = Joy(title: "") /// 오늘의 소확행
-    @State private var todayJoyId: Int = UserDefaults.standard.integer(forKey: "todayJoyId") /// 클릭 여부
+    @AppStorage("todayJoyId") var todayJoyId = 0
+//    @State private var todayJoyId: Int = UserDefaults.standard.integer(forKey: "todayJoyId") /// 클릭 여부
     @State private var counter = 0 /// 파티클 애니메이션 추가
     @State var selectedJoy: Joy? /// 소확행 메뉴 bottom sheet 연결 joy
     
     @Binding var updatePlaylistBar: Bool /// 플리바 업데이트
     
+    @State private var isFinishedGetJoy: Bool = false
+    
     var body: some View {
         ZStack {
             VStack(alignment: .leading, spacing: 0) {
-                if todayJoyId == todayJoy.joyId {
-                    JoyRowWithButton(joy: todayJoy) { } buttonLabel: {
-                        Button {
-                            // 소확행 오플리에 추가하기
-                            playJoy()
-                        } label: {
-                            Image("play")
-                                .resizable()
-                                .foregroundColor(.gray300)
-                                .frame(width: 16, height: 18)
+                if isFinishedGetJoy {
+                    if todayJoyId == todayJoy.joyId {
+                        JoyRowWithButton(joy: todayJoy) { } buttonLabel: {
+                            Button {
+                                // 소확행 오플리에 추가하기
+                                playJoy()
+                            } label: {
+                                Image("play")
+                                    .resizable()
+                                    .foregroundColor(.gray300)
+                                    .frame(width: 16, height: 18)
+                            }
+                            
+                            Button {
+                                // 소확행 메뉴 bottom sheet
+                                selectedJoy = todayJoy
+                            } label: {
+                                Image(systemName: "ellipsis")
+                                    .foregroundColor(.gray500)
+                                    .rotationEffect(Angle(degrees: 90))
+                            }
+                            .sheet(item: $selectedJoy) { _ in
+                                JoyMenuBottomSheet(joy: $selectedJoy, isMine: false, isFromTodayJoy: true) }
                         }
-                        
-                        Button {
-                            // 소확행 메뉴 bottom sheet
-                            selectedJoy = todayJoy
-                        } label: {
-                            Image(systemName: "ellipsis")
-                                .foregroundColor(.gray500)
-                                .rotationEffect(Angle(degrees: 90))
-                        }
-                        .sheet(item: $selectedJoy) { _ in
-                            JoyMenuBottomSheet(joy: $selectedJoy, isMine: false, isFromTodayJoy: true) }
+                    } else {
+                        // 클릭해보세요! 버튼
+                        TodayJoyBeforeClickButton(todayJoyId: $todayJoyId, counter: $counter, todayJoy: $todayJoy)
                     }
-                } else {
-                    // 클릭해보세요! 버튼
-                    TodayJoyBeforeClickButton(todayJoyId: $todayJoyId, counter: $counter, todayJoy: $todayJoy)
                 }
             }
             .frame(height: 56)
@@ -64,6 +69,7 @@ struct HomeTodayJoyView: View {
             if isSuccess {
                 print("DEBUG HomeTodayJoyView getTodayJoy isSuccess true, \(todayJoy)")
                 self.todayJoy = Joy(joyId: todayJoy.joyId, icon: todayJoy.joyIconNum, title: todayJoy.contents)
+                isFinishedGetJoy = true
             } else {
                 print("DEBUG HomeTodayJoyView getTodayJoy isSuccess false")
             }
@@ -81,4 +87,3 @@ struct HomeTodayJoyView: View {
         }
     }
 }
-
