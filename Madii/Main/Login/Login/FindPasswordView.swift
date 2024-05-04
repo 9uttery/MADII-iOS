0dd6d7e53d33835a72fe223207c1b6b4108d2da1
Anjin Lee <anjin720jin@naver.com>
2024-05-04 13:16:19 +0900

    Feat: 비밀번호 찾기 화면 생성

---
+++ Madii/Main/Login/Login/FindPasswordView.swift
@@ -0,0 +1,22 @@
+//
+//  FindPasswordView.swift
+//  Madii
+//
+//  Created by Anjin on 5/2/24.
+//
+
+import SwiftUI
+
+struct FindPasswordView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(spacing: 0) {
+                Image(systemName: "chevron.leading")
+            }
+        }
+    }
+}
+
+#Preview {
+    FindPasswordView()
+}
