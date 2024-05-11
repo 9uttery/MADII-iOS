e5352c3c8d9b9911a643c326960da5683a8ac6c4
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2024-05-11 17:42:33 +0900

    feat: appstatus 프로퍼티 추가

--- Madii/Service/AppStatus.swift
+++ Madii/Service/AppStatus.swift
@@ -11,4 +11,6 @@ class AppStatus: ObservableObject {
     @Published var showReportToast: Bool = false /// 신고 토스트
     @Published var showAddPlaylistToast: Bool = false /// 오플리 추가 토스트
     @Published var showSaveAlbumToast: Bool = false /// 앨범 저장 토스트
+    @Published var isDuplicate: Bool = false /// 오플리 중복 토스트
+    @Published var showSaveJoyToast: Bool = false /// 소확행 앨범에 저장 토스트
 }
