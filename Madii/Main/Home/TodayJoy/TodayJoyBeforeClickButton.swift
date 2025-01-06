a09d5ad30cdad1ccbbb05bdef0a74dea2179cc77
taewoojeong <97501702+taewoojeong@users.noreply.github.com>
2025-01-06 23:27:52 +0900

    Fix: GA오류 수정하기

--- Madii/Main/Home/TodayJoy/TodayJoyBeforeClickButton.swift
+++ Madii/Main/Home/TodayJoy/TodayJoyBeforeClickButton.swift
@@ -16,7 +16,7 @@ struct TodayJoyBeforeClickButton: View {
             todayJoyId = todayJoy.joyId ?? 0
             UserDefaults.standard.set(todayJoy.joyId, forKey: "todayJoyId")
             counter += 1
-            AnalyticsManager.shared.logEvent(name: "홈뷰_클릭해보세요!클릭")
+            AnalyticsManager.shared.logEvent(name: "홈뷰_클릭해보세요클릭")
             HapticManager.instance.notification(type: .success)
         } label: {
             ZStack {
