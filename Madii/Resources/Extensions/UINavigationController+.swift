b28f2936a5e588a79aded18ca1a74db187b4357d
anjiniii <anjin720jin@naver.com>
2025-09-27 15:58:01 +0900

    feat(Router): 뒤로가기 제스처 인식 추가 구현

---
+++ Madii/Resources/Extensions/UINavigationController+.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+.swift
+//  Madii
+//
+//  Created by Anjin on 9/27/25.
+//
+
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
