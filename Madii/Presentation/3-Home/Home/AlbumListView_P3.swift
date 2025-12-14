c2dba7c2439dc1bb952fd56bf33ca129e6a7cb42
정태우 <jeongtaewoo5037@gmail.com>
2025-12-14 16:09:22 +0900

    fix: 앨범 삭제 시에도 생성 가능하게 변경

--- Madii/Presentation/3-Home/Home/AlbumListView_P3.swift
+++ Madii/Presentation/3-Home/Home/AlbumListView_P3.swift
@@ -5,147 +5,155 @@
 //  Created by 정태우 on 7/21/25.
 //
 
+import MadiiDesignSystem
 import SwiftUI
 
 struct AlbumListView_P3: View {
     @State private var viewModel: AlbumListViewModel_P3
     @State var showDeleteAlbumsBottomSheet: Bool = false
     @State var isDismiss: Bool = false
+    @State var showSuccessToast: Bool = false
     
     init(viewModel: AlbumListViewModel_P3) {
         _viewModel = State(initialValue: viewModel)
     }
     
     var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    viewModel.action(viewModel.isSelect ? .toggleSelect : .popView)
-                } label: {
-                    if viewModel.isSelect {
-                        Text("취소")
-                            .madiiFont(font: .madiiBody3, color: .madiiNormal)
-                            .padding(.vertical, 4)
-                            .padding(.horizontal, 12)
-                            .background(.madiiContrast)
-                            .cornerRadius(10)
-                    } else {
-                        Image("arrowBack")
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                            .opacity(0.43)
+        ZStack(alignment: .bottom) {
+            VStack {
+                HStack {
+                    Button {
+                        viewModel.action(viewModel.isSelect ? .toggleSelect : .popView)
+                    } label: {
+                        if viewModel.isSelect {
+                            Text("취소")
+                                .madiiFont(font: .madiiBody3, color: .madiiNormal)
+                                .padding(.vertical, 4)
+                                .padding(.horizontal, 12)
+                                .background(.madiiContrast)
+                                .cornerRadius(10)
+                        } else {
+                            Image("arrowBack")
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .opacity(0.43)
+                        }
                     }
-                }
-                
-                Spacer()
-                
-                Text("행복 앨범")
-                    .madiiFont(font: .madiiSubTitle, color: .white.opacity(0.97))
-                
-                Spacer()
-                
-                Button {
-                    viewModel.isSelect
+                    
+                    Spacer()
+                    
+                    Text("행복 앨범")
+                        .madiiFont(font: .madiiSubTitle, color: .white.opacity(0.97))
+                    
+                    Spacer()
+                    
+                    Button {
+                        viewModel.isSelect
                         ? showDeleteAlbumsBottomSheet = true
                         : viewModel.action(.toggleSelect)
-                } label: {
-                    if viewModel.isSelect {
-                        Text("삭제")
-                            .madiiFont(font: .madiiBody3, color: .madiiStrong)
-                            .padding(.vertical, 4)
-                            .padding(.horizontal, 12)
-                            .background(viewModel.selectedAlbums.isEmpty ? .madiiContrast : .madiiNegative)
-                            .cornerRadius(10)
-                    } else {
-                        Text("편집")
-                            .madiiFont(font: .madiiBody3, color: .madiiNormal)
-                            .padding(.vertical, 4)
-                            .padding(.horizontal, 12)
-                            .background(.madiiBox)
-                            .cornerRadius(10)
+                    } label: {
+                        if viewModel.isSelect {
+                            Text("삭제")
+                                .madiiFont(font: .madiiBody3, color: .madiiStrong)
+                                .padding(.vertical, 4)
+                                .padding(.horizontal, 12)
+                                .background(viewModel.selectedAlbums.isEmpty ? .madiiContrast : .madiiNegative)
+                                .cornerRadius(10)
+                        } else {
+                            Text("편집")
+                                .madiiFont(font: .madiiBody3, color: .madiiNormal)
+                                .padding(.vertical, 4)
+                                .padding(.horizontal, 12)
+                                .background(.madiiBox)
+                                .cornerRadius(10)
+                        }
                     }
                 }
-            }
-            .padding(.horizontal, 20)
-            .frame(height: 64)
-            
-            ScrollView {
-                LazyVGrid(columns: Array(repeating: .init(.flexible(), spacing: 0, alignment: .top), count: 2), spacing: 24) {
-                    VStack(alignment: .leading, spacing: 12) {
-                        Button {
-                            viewModel.action(.createAlbum)
-                        } label: {
-                            Image("plusStroke")
-                                .resizable()
-                                .foregroundStyle(.madiiGreen100)
-                                .frame(width: 48, height: 48)
-                                .padding(UIScreen.main.bounds.width / 4 - 37)
-                                .background(.gray100.opacity(0.08))
-                                .cornerRadius(32)
-                        }
-                        
-                        Text("새로운 앨범")
-                            .madiiFont(font: .madiiBody4, color: .madiiGreen100)
-                            .frame(height: 22, alignment: .center)
-                            .padding(.horizontal, 8)
-                    }
-                    .frame(height: UIScreen.main.bounds.width / 2 + 8)
-                    
-                    ForEach(viewModel.albums) { album in
-                        Button {
-                            if viewModel.isSelect {
-                                viewModel.action(.selectAlbum(album: album))
-                            } else {
-                                viewModel.action(.showAlbumDetail(album: album))
+                .padding(.horizontal, 20)
+                .frame(height: 64)
+                
+                ScrollView {
+                    LazyVGrid(columns: Array(repeating: .init(.flexible(), spacing: 0, alignment: .top), count: 2), spacing: 24) {
+                        VStack(alignment: .leading, spacing: 12) {
+                            Button {
+                                viewModel.action(.createAlbum)
+                            } label: {
+                                Image("plusStroke")
+                                    .resizable()
+                                    .foregroundStyle(.madiiGreen100)
+                                    .frame(width: 48, height: 48)
+                                    .padding(UIScreen.main.bounds.width / 4 - 37)
+                                    .background(.gray100.opacity(0.08))
+                                    .cornerRadius(32)
                             }
-                        } label: {
-                            VStack(alignment: .leading, spacing: 12) {
-                                ZStack(alignment: .topTrailing) {
-                                    Image("Cover\(album.backgroundColorNum)")
-                                        .resizable()
-                                        .frame(width: (UIScreen.main.bounds.width - 52) / 2, height: (UIScreen.main.bounds.width - 52) / 2)
-                                        .cornerRadius(32)
-                                    
-                                    if viewModel.isSelect {
-                                        if viewModel.selectedAlbums.contains(album) {
-                                            Image(systemName: "checkmark.circle")
-                                                .resizable()
-                                                .frame(width: 24, height: 24)
-                                                .padding([.top, .trailing], 14)
-                                        } else {
-                                            Image(systemName: "circle")
-                                                .resizable()
-                                                .frame(width: 24, height: 24)
-                                                .padding([.top, .trailing], 14)
+                            
+                            Text("새로운 앨범")
+                                .madiiFont(font: .madiiBody4, color: .madiiGreen100)
+                                .frame(height: 22, alignment: .center)
+                                .padding(.horizontal, 8)
+                        }
+                        .frame(height: UIScreen.main.bounds.width / 2 + 8)
+                        
+                        ForEach(viewModel.albums) { album in
+                            Button {
+                                if viewModel.isSelect {
+                                    viewModel.action(.selectAlbum(album: album))
+                                } else {
+                                    viewModel.action(.showAlbumDetail(album: album))
+                                }
+                            } label: {
+                                VStack(alignment: .leading, spacing: 12) {
+                                    ZStack(alignment: .topTrailing) {
+                                        Image("Cover\(album.backgroundColorNum)")
+                                            .resizable()
+                                            .frame(width: (UIScreen.main.bounds.width - 52) / 2, height: (UIScreen.main.bounds.width - 52) / 2)
+                                            .cornerRadius(32)
+                                        
+                                        if viewModel.isSelect {
+                                            if viewModel.selectedAlbums.contains(album) {
+                                                Image(systemName: "checkmark.circle")
+                                                    .resizable()
+                                                    .frame(width: 24, height: 24)
+                                                    .padding([.top, .trailing], 14)
+                                            } else {
+                                                Image(systemName: "circle")
+                                                    .resizable()
+                                                    .frame(width: 24, height: 24)
+                                                    .padding([.top, .trailing], 14)
+                                            }
                                         }
                                     }
+                                    
+                                    Text(album.title)
+                                        .madiiFont(font: .madiiBody3, color: .white)
+                                        .multilineTextAlignment(.leading)
+                                        .frame(maxWidth: 152, alignment: .leading)
+                                        .padding(.horizontal, 8)
                                 }
-                                
-                                Text(album.title)
-                                    .madiiFont(font: .madiiBody3, color: .white)
-                                    .multilineTextAlignment(.leading)
-                                    .frame(maxWidth: 152, alignment: .leading)
-                                    .padding(.horizontal, 8)
+                                .opacity(!viewModel.isSelect || viewModel.selectedAlbums.contains(album) ? 1.0 : 0.4)
                             }
-                            .opacity(!viewModel.isSelect || viewModel.selectedAlbums.contains(album) ? 1.0 : 0.4)
                         }
                     }
                 }
-            }
-            .scrollIndicators(.hidden)
-            .onChange(of: showDeleteAlbumsBottomSheet) {
-                viewModel.action(.loadAlbums)
-            }
-            .onChange(of: viewModel.showNewAlbumBottomSheet) {
-                viewModel.action(.loadAlbums)
-            }
-            .onChange(of: isDismiss) {
-                if isDismiss {
-                    viewModel.isSelect = false
+                .scrollIndicators(.hidden)
+                .onChange(of: showDeleteAlbumsBottomSheet) {
+                    viewModel.action(.loadAlbums)
+                }
+                .onChange(of: viewModel.showNewAlbumBottomSheet) {
+                    viewModel.action(.loadAlbums)
+                }
+                .onChange(of: isDismiss) {
+                    if isDismiss {
+                        viewModel.isSelect = false
+                    }
+                }
+                .onAppear {
+                    viewModel.action(.loadAlbums)
                 }
             }
-            .onAppear {
-                viewModel.action(.loadAlbums)
+            if showSuccessToast {
+                MadiiDesignSystem.MadiiToast(title: "앨범에 저장되었어요", isShowToast: $showSuccessToast)
+                    .padding(.bottom, 100)
             }
         }
         .opacity(showDeleteAlbumsBottomSheet || viewModel.showNewAlbumBottomSheet ? 0.8 : 1)
@@ -159,7 +167,7 @@ struct AlbumListView_P3: View {
         }
         .sheet(isPresented: $viewModel.showNewAlbumBottomSheet) {
             GeometryReader { geo in
-                AddNewAlbumBottomSheet(showAddNewAlbumBottomSheet: $viewModel.showNewAlbumBottomSheet, album: .constant(Album(id: 0, title: "")))
+                AddNewAlbumBottomSheet(showAddNewAlbumBottomSheet: $viewModel.showNewAlbumBottomSheet, showSuccessToast: $showSuccessToast, album: .constant(Album(id: 0, title: "")))
                     .presentationDetents([.height(503)])
                     .presentationDragIndicator(.hidden)
                     .presentationBackground(.clear)
